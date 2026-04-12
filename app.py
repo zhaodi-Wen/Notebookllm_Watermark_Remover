375c72caa88449c0698b4acc337244896678c52d
zhaodi-Wen <zhaodi.wen@gmail.com>
2026-04-12 16:06:26 +0800

    debug: add console.log to loadPreview, fix no-cache header

--- app.py
+++ app.py
@@ -723,7 +723,11 @@ def _blob_delete(blob_url):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    from flask import make_response
+    resp = make_response(render_template('index.html'))
+    resp.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    resp.headers['Pragma'] = 'no-cache'
+    return resp
 
 
 ALLOWED_PDF = {'.pdf'}
